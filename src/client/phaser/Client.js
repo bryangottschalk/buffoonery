4970f84e8fc7fd1b3337bf59d3c8e368f9a84bbb
bryangottschalk <bryanlgottschalk@gmail.com>
2020-11-30 23:21:01 -0600

    generate roomcode or get from url

--- src/client/phaser/Client.js
+++ src/client/phaser/Client.js
@@ -4,6 +4,7 @@ import BootScene from './scenes/BootScene';
 import PreloaderScene from './scenes/PreloaderScene';
 import GameOverScene from './scenes/GameOverScene';
 import MainScene from './scenes/MainScene';
+import { v1 as uuidv1 } from 'uuid';
 
 const config = {
   type: Phaser.AUTO,
@@ -18,12 +19,40 @@ const config = {
   },
 };
 
+const generateRoomCode = (length) => {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+
 class Client extends Phaser.Game {
   constructor() {
     super(config);
     // configure websocket
-    this.ws = new WebSocket("wss://camoniwkva.execute-api.us-east-1.amazonaws.com/dev");
-    this.ws.onopen = () => this.ws.send(JSON.stringify({action: 'sendmessage', data: 'first test message'}))
+    
+    const url = window.location.href;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const roomcode = urlParams.get('roomcode') | generateRoomCode(4)
+  
+    this.ws = new WebSocket(`wss://oyvtlf0pch.execute-api.us-east-1.amazonaws.com/dev?roomcode=${roomcode}`);
+
+    this.ws.onopen = () => {
+      const roomcode = urlParams.get('roomcode')
+      if (!roomcode) {
+        console.log('no roomcode from URL, creating gameroom id...')
+        //const msg = JSON.stringify({action: 'sendmessage', gameroomId: uuidv1(), roomcode: roomcode, data: 'new gameroom created'})
+        //this.ws.send(msg);
+      } else {
+        console.log('roomcode detected from URL... getting existing room')
+      }
+    }
+    // this.ws.onopen = event => new SocketMessage(event);
+    // this.ws.onerror = event => new SocketError(event);
+    // this.ws.onmessage = event => new SocketMessage(event);
+    // this.ws.onclose = event => new SocketClose(event);
+    
     // configure scenes
     this.scene.add('Home', HomeScene);
     this.scene.add('Boot', BootScene);
