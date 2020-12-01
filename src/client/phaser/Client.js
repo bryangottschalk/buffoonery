52b2aea1b7877cdfa79aa52c44f8b8d4b0c8798d
bryangottschalk <bryanlgottschalk@gmail.com>
2020-11-30 20:14:32 -0600

    connect to websocket

--- src/client/phaser/Client.js
+++ src/client/phaser/Client.js
@@ -21,13 +21,16 @@ const config = {
 class Client extends Phaser.Game {
   constructor() {
     super(config);
+    // configure websocket
+    this.ws = new WebSocket("wss://camoniwkva.execute-api.us-east-1.amazonaws.com/dev");
+    this.ws.onopen = () => this.ws.send(JSON.stringify({action: 'sendmessage', data: 'first test message'}))
+    // configure scenes
     this.scene.add('Home', HomeScene);
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('GameOver', GameOverScene);
     this.scene.add('Main', MainScene);
     this.scene.start('Boot');
-
     // 1) starts boot scene
     // 2) boot scene starts preloader
     // 3) preloader starts game
