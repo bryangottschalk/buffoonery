52fda515584878f0b3f8ecb719570e1c0b8edcd9
bryangottschalk <bryanlgottschalk@gmail.com>
2020-12-12 14:57:16 -0500

    move all websocket logic to HomeScene

--- src/client/phaser/Client.js
+++ src/client/phaser/Client.js
@@ -5,7 +5,6 @@ import PreloaderScene from './scenes/PreloaderScene';
 import GameOverScene from './scenes/GameOverScene';
 import MainScene from './scenes/MainScene';
 import { v1 as uuidv1 } from 'uuid';
-import axios from 'axios';
 
 const config = {
   type: Phaser.AUTO,
@@ -20,149 +19,9 @@ const config = {
   }
 };
 
-const generateRoomCode = (length) => {
-  const chars =
-    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var result = '';
-  for (var i = length; i > 0; --i)
-    result += chars[Math.floor(Math.random() * chars.length)];
-  return result;
-};
-
-const setPlayersInLobby = (numPlayersConnected) => {
-  console.log('setting player count:', numPlayersConnected);
-  const parent = document.getElementById('num-players');
-  if (parent.childElementCount > 0) {
-    document.getElementById('test').innerHTML = numPlayersConnected;
-  } else {
-    const el = document.createElement('div');
-    el.id = 'test';
-    el.innerHTML = numPlayersConnected;
-    parent.appendChild(el);
-  }
-};
-
-const addMessage = (comment) => {
-  const parent = document.getElementById('events');
-  const el = document.createElement('li');
-  el.innerHTML = `${comment.name}: ${comment.chatMsg}`;
-  parent.appendChild(el);
-};
-
-const setInitialChat = (comments) => {
-  const parent = document.getElementById('events');
-  comments.forEach((c) => {
-    const el = document.createElement('li');
-    el.innerHTML = `${c.name}: ${c.chatMsg}`;
-    parent.appendChild(el);
-  });
-};
-
 class Client extends Phaser.Game {
   constructor() {
     super(config);
-    // configure websocket
-    const url = window.location.href;
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    this.roomcode =
-      urlParams.get('roomcode') || generateRoomCode(4).toUpperCase();
-    const devUrl = `wss://da6wisihu2.execute-api.us-east-1.amazonaws.com/dev?roomcode=${
-      this.roomcode
-    }&isHost=${true}&name=${'Host'}`;
-
-    this.ws = new WebSocket(`${devUrl}`);
-    window.history.pushState('', 'Buffoonery', `?roomcode=${this.roomcode}`);
-
-    this.ws.onopen = async () => {
-      this.state = {
-        comments: [],
-        connectedClients: [],
-        roomcode: ''
-      };
-      // var connectMsg = {
-      //   action: 'sendmessage',
-      //   data: {
-      //     msg: `CONNECTION OPENED IN ROOMCODE: ${this.roomcode}`,
-      //     roomcode: this.roomcode
-      //   }
-      // };
-      // this.ws.send(JSON.stringify(connectMsg));
-
-      // get initial room state
-      try {
-        const { data } = await axios.get(
-          `https://dev-api.buffoonery.io/getmeetingstate/${this.roomcode}`
-        );
-        console.log('INITIAL MEETING STATE', data);
-        this.state = data;
-        if (data) {
-          setPlayersInLobby(this.state.connectedClients.length);
-          setInitialChat(data.comments);
-        }
-      } catch (err) {
-        console.error('error getting intial meeting state:', err);
-      }
-    };
-    this.ws.onclose = () => {
-      // var disconnectMsg = { action: 'sendmessage', data: 'DISCONNECT' };
-      // this.ws.send(JSON.stringify(disconnectMsg));
-    };
-    // this.ws.onopen = event => new SocketMessage(event);
-    // this.ws.onerror = event => new SocketError(event);
-    this.ws.onmessage = async (event) => {
-      let msg = JSON.parse(event.data);
-      if (typeof msg === 'string') {
-        msg = JSON.parse(msg);
-      }
-      const doesContainClientId = (clientIdToCheck) =>
-        this.state.connectedClients.some((clientObj) => {
-          return clientObj.connectionId === clientIdToCheck;
-        });
-      console.log('RECEIVED MESSAGE FROM SERVER:', msg);
-      console.log('topic', msg.topic);
-      if (msg && msg.topic) {
-        console.log('topic:', msg.topic);
-        switch (msg.topic) {
-          case 'Client Connected':
-            if (msg.name === 'Host') {
-              setTimeout(() => {
-                if (!doesContainClientId(msg.client.connectionId)) {
-                  this.state.connectedClients.push(msg.client);
-                  setPlayersInLobby(this.state.connectedClients.length);
-                }
-              }, 1000);
-            } else {
-              if (!doesContainClientId(msg.client.connectionId)) {
-                this.state.connectedClients.push(msg.client);
-                setPlayersInLobby(this.state.connectedClients.length);
-              }
-            }
-
-            break;
-          case 'Client Disconnected':
-            if (doesContainClientId(msg.connectionId)) {
-              this.state.connectedClients = this.state.connectedClients.filter(
-                (c) => c.connectionId !== msg.connectionId
-              );
-              setPlayersInLobby(this.state.connectedClients.length);
-            }
-            break;
-          case 'Comment Received':
-            console.log('add to chat messages...');
-            addMessage(msg.comment);
-            break;
-          default:
-            throw new Error(`Unhandled topic from server: ${msg.topic}`);
-        }
-      }
-      console.log('GAME STATE', this.state);
-    };
-    this.ws.onclose = (event) => {
-      // const msg = JSON.stringify({action: 'sendmessage', roomcode: this.roomcode})
-      // this.ws.send(msg)
-    };
-
     // configure scenes
     this.scene.add('Home', HomeScene);
     this.scene.add('Boot', BootScene);
--- src/client/phaser/scenes/HomeScene.js
+++ src/client/phaser/scenes/HomeScene.js
@@ -4,6 +4,46 @@ import createBumpers from '../customFunctions/createBumpers';
 import getBall from '../customFunctions/getBall';
 import getBumpers from '../customFunctions/getBumpers';
 import checkWin from '../customFunctions/checkWin';
+import { v1 as uuidv1 } from 'uuid';
+import axios from 'axios';
+
+const generateRoomCode = (length) => {
+  const chars =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
+
+const setPlayersInLobby = (numPlayersConnected) => {
+  console.log('setting player count:', numPlayersConnected);
+  const parent = document.getElementById('num-players');
+  if (parent.childElementCount > 0) {
+    document.getElementById('test').innerHTML = numPlayersConnected;
+  } else {
+    const el = document.createElement('div');
+    el.id = 'test';
+    el.innerHTML = numPlayersConnected;
+    parent.appendChild(el);
+  }
+};
+
+const addMessage = (comment) => {
+  const parent = document.getElementById('events');
+  const el = document.createElement('li');
+  el.innerHTML = `${comment.name}: ${comment.chatMsg}`;
+  parent.appendChild(el);
+};
+
+const setInitialChat = (comments) => {
+  const parent = document.getElementById('events');
+  comments.forEach((c) => {
+    const el = document.createElement('li');
+    el.innerHTML = `${c.name}: ${c.chatMsg}`;
+    parent.appendChild(el);
+  });
+};
 
 class HomeScene extends Phaser.Scene {
   constructor() {
@@ -90,7 +130,109 @@ class HomeScene extends Phaser.Scene {
     this.scene.start('Main');
   }
   create() {
-    console.log('ws', this.game.ws);
+    const game = window.game;
+    // configure websocket
+    const url = window.location.href;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    game.roomcode =
+      urlParams.get('roomcode') || generateRoomCode(4).toUpperCase();
+    const devUrl = `wss://da6wisihu2.execute-api.us-east-1.amazonaws.com/dev?roomcode=${
+      game.roomcode
+    }&isHost=${true}&name=${'Host'}`;
+
+    game.ws = new WebSocket(`${devUrl}`);
+    window.history.pushState('', 'Buffoonery', `?roomcode=${game.roomcode}`);
+
+    game.ws.onopen = async () => {
+      game.state = {
+        comments: [],
+        connectedClients: [],
+        roomcode: ''
+      };
+      // var connectMsg = {
+      //   action: 'sendmessage',
+      //   data: {
+      //     msg: `CONNECTION OPENED IN ROOMCODE: ${game.roomcode}`,
+      //     roomcode: game.roomcode
+      //   }
+      // };
+      // game.ws.send(JSON.stringify(connectMsg));
+
+      // get initial room state
+      try {
+        const { data } = await axios.get(
+          `https://dev-api.buffoonery.io/getmeetingstate/${game.roomcode}`
+        );
+        console.log('INITIAL MEETING STATE', data);
+        game.state = data;
+        if (data) {
+          setPlayersInLobby(game.state.connectedClients.length);
+          setInitialChat(data.comments);
+        }
+      } catch (err) {
+        console.error('error getting intial meeting state:', err);
+      }
+    };
+    game.ws.onclose = () => {
+      // var disconnectMsg = { action: 'sendmessage', data: 'DISCONNECT' };
+      // game.ws.send(JSON.stringify(disconnectMsg));
+    };
+    // game.ws.onopen = event => new SocketMessage(event);
+    // game.ws.onerror = event => new SocketError(event);
+    game.ws.onmessage = async (event) => {
+      let msg = JSON.parse(event.data);
+      if (typeof msg === 'string') {
+        msg = JSON.parse(msg);
+      }
+      const doesContainClientId = (clientIdToCheck) =>
+        game.state.connectedClients.some((clientObj) => {
+          return clientObj.connectionId === clientIdToCheck;
+        });
+      console.log('RECEIVED MESSAGE FROM SERVER:', msg);
+      console.log('topic', msg.topic);
+      if (msg && msg.topic) {
+        console.log('topic:', msg.topic);
+        switch (msg.topic) {
+          case 'Client Connected':
+            if (msg.name === 'Host') {
+              setTimeout(() => {
+                if (!doesContainClientId(msg.client.connectionId)) {
+                  game.state.connectedClients.push(msg.client);
+                  setPlayersInLobby(game.state.connectedClients.length);
+                }
+              }, 1000);
+            } else {
+              if (!doesContainClientId(msg.client.connectionId)) {
+                game.state.connectedClients.push(msg.client);
+                setPlayersInLobby(game.state.connectedClients.length);
+              }
+            }
+
+            break;
+          case 'Client Disconnected':
+            if (doesContainClientId(msg.connectionId)) {
+              game.state.connectedClients = game.state.connectedClients.filter(
+                (c) => c.connectionId !== msg.connectionId
+              );
+              setPlayersInLobby(game.state.connectedClients.length);
+            }
+            break;
+          case 'Comment Received':
+            console.log('add to chat messages...');
+            addMessage(msg.comment);
+            break;
+          default:
+            throw new Error(`Unhandled topic from server: ${msg.topic}`);
+        }
+      }
+      console.log('GAME STATE', game.state);
+    };
+    game.ws.onclose = (event) => {
+      // const msg = JSON.stringify({action: 'sendmessage', roomcode: game.roomcode})
+      // game.ws.send(msg)
+    };
+
     this.music = this.sound.add('startup-music');
     const musicConfig = {
       mute: false,
@@ -117,7 +259,7 @@ class HomeScene extends Phaser.Scene {
     this.text1 = this.add.text(100, 200, 'Buffoonery', {
       fontSize: '100px'
     });
-    this.text2 = this.add.text(100, 300, `Room code: ${this.game.roomcode}`, {
+    this.text2 = this.add.text(100, 300, `Room code: ${game.roomcode}`, {
       fontSize: '50px'
     });
     this.text3 = this.add.text(
@@ -129,21 +271,21 @@ class HomeScene extends Phaser.Scene {
       }
     );
     // SET PLAYERS
-    setTimeout(() => {
-      this.player1 = this.physics.add.sprite(150, 800, 'fighter');
-      this.player1.displayWidth = 175;
-      this.player1.displayHeight = 175;
-    }, 2000);
-    setTimeout(() => {
-      this.player2 = this.physics.add.sprite(350, 800, 'archer');
-      this.player2.displayWidth = 175;
-      this.player2.displayHeight = 175;
-    }, 3000);
-    setTimeout(() => {
-      this.player3 = this.physics.add.sprite(550, 800, 'wizard');
-      this.player3.displayWidth = 175;
-      this.player3.displayHeight = 175;
-    }, 4000);
+    // setTimeout(() => {
+    //   this.player1 = this.physics.add.sprite(150, 800, 'fighter');
+    //   this.player1.displayWidth = 175;
+    //   this.player1.displayHeight = 175;
+    // }, 2000);
+    // setTimeout(() => {
+    //   this.player2 = this.physics.add.sprite(350, 800, 'archer');
+    //   this.player2.displayWidth = 175;
+    //   this.player2.displayHeight = 175;
+    // }, 3000);
+    // setTimeout(() => {
+    //   this.player3 = this.physics.add.sprite(550, 800, 'wizard');
+    //   this.player3.displayWidth = 175;
+    //   this.player3.displayHeight = 175;
+    // }, 4000);
   }
 
   update() {}
