190bf9429b0b101791afce2a50cb0b3914980ff3
bryangottschalk <bryanlgottschalk@gmail.com>
2021-01-02 15:12:02 -0500

    #1 narrator intro updates

--- src/client/phaser/scenes/MainScene.js
+++ src/client/phaser/scenes/MainScene.js
@@ -1,12 +1,18 @@
 import Phaser from 'phaser';
 
+const addVoiceConfig = (utterance, voices, name, rate, pitch) => {
+  utterance.voice = voices.find((v) => v.name === name);
+  utterance.rate = rate;
+  utterance.pitch = pitch;
+};
+
 const initNarratorIntro = async () => {
   window.speechSynthesis.onvoiceschanged = function () {
     const synth = window.speechSynthesis;
     const voices = window.speechSynthesis.getVoices();
     const getGreeting = () => {
-      var today = new Date();
-      var curHr = today.getHours();
+      const today = new Date();
+      const curHr = today.getHours();
       if (curHr < 12) {
         return 'good morning';
       } else if (curHr < 18) {
@@ -18,25 +24,19 @@ const initNarratorIntro = async () => {
     const msg1 = new SpeechSynthesisUtterance(
       `${getGreeting()} friends, welcome to Buffoonery, a game of mass chaos and destruction.`
     );
-    msg1.voice = voices.find((v) => v.name === 'Daniel');
-    msg1.rate = 7;
-    msg1.pitch = 1;
+    addVoiceConfig(msg1, voices, 'Daniel', 7, 1);
     const msg2 = new SpeechSynthesisUtterance(
-      `The rules are simple. Prompts will appear on your phone in which you will fill in the blank with your silliest, wackiest quips. After all prompts are submitted, the users that did not receive your prompts will be able to vote on which response they like best. The more votes you get, the higher your score will be.`
+      `The rules are simple. Prompts will appear on your phone in which you will fill in the blank with your silliest, wackiest quips. After all prompts are submitted, the users that did not receive your prompts will be able to vote on which response they like best.`
     );
-    msg2.voice = voices.find((v) => v.name === 'Daniel');
-    msg2.rate = 7;
-    msg2.pitch = 1;
+    addVoiceConfig(msg2, voices, 'Daniel', 7, 1);
     const msg3 = new SpeechSynthesisUtterance(
+      `The more votes you get, the higher your score will be.`
+    );
+    addVoiceConfig(msg3, voices, 'Daniel', 7, 1);
+    const msg4 = new SpeechSynthesisUtterance(
       `Without further ado, let's get jiggy with it and start the game.`
     );
-    msg3.voice = voices.find((v) => v.name === 'Daniel');
-    msg3.rate = 7;
-    msg3.pitch = 1;
-
-    msg2.voice = voices.find((v) => v.name === 'Daniel');
-    msg2.rate = 7;
-    msg2.pitch = 1;
+    addVoiceConfig(msg4, voices, 'Daniel', 7, 1);
     synth.speak(msg1);
     msg1.onend = function (e) {
       setTimeout(() => {
@@ -46,6 +46,11 @@ const initNarratorIntro = async () => {
     msg2.onend = function (e) {
       setTimeout(() => {
         synth.speak(msg3);
+      }, 500);
+    };
+    msg3.onend = function (e) {
+      setTimeout(() => {
+        synth.speak(msg4);
       }, 500);
     };
   };
